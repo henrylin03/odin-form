be75729f390994996837a160dfd1275d98eb5cdc
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-06-12 19:40:54 +1000

    add required to email and first use guard clause to confirm the email field is valid

--- script.js
+++ script.js
@@ -7,7 +7,16 @@ form.addEventListener("submit", (e) => {
 });
 
 function validateEmail() {
-  const errorMessage = email.parentNode.querySelector(".error-message");
-  console.log(errorMessage);
+  const container = email.parentNode;
+  const error = container.querySelector(".error-message");
+
+  if (email.validity.valid) {
+    error.textContent = "";
+    container.classList.remove("error");
+    console.log("all good");
+    return;
+  }
+  if (email.validity.valueMissing)
+    error.textContent = "This is a required field";
   // if (email.validity.valueMissing)
 }
