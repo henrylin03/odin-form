d63a52d727e27c4c4bde6a4ea55baba11c181e14
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-06-12 19:36:29 +1000

    select the error message for email

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const form = document.querySelector("form");
+const email = document.querySelector("#email");
 
-form.addEventListener("submit", (e) => e.preventDefault());
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validateEmail();
+});
+
+function validateEmail() {
+  const errorMessage = email.parentNode.querySelector(".error-message");
+  console.log(errorMessage);
+  // if (email.validity.valueMissing)
+}
